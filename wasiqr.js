4d1f2735f7dba611a54276f56dda6894a31c0f08
romeomdbot <newheroku075@gmail.com>
2025-02-17 17:01:48 +0500

    Update wasiqr.js

--- wasiqr.js
+++ wasiqr.js
@@ -59,7 +59,7 @@ router.get('/', async (req, res) => {
 				   let session = await Qr_Code_By_Wasi_Tech.sendMessage(Qr_Code_By_Wasi_Tech.user.id, { text: '' + b64data });
 	
 				   let WASI_MD_TEXT = `
-*_Session Connected By Wasi Tech_*
+*_Session Connected By ROMEO-MD_*
 *_Made With 🤍_*
 ______________________________________
 ╔════◇
@@ -68,12 +68,12 @@ ______________________________________
 ╚════════════════════════╝
 ╔═════◇
 ║  『••• 𝗩𝗶𝘀𝗶𝘁 𝗙𝗼𝗿 𝗛𝗲𝗹𝗽 •••』
-║❒ *Ytube:* _youtube.com/@wasitech1
+║❒ *Ytube:* _
 ║❒ *Owner:* _https://wa.me/message/THZ3I25BYZM2E1_
-║❒ *Repo:* _https://github.com/wasixd/WASI-MD_
-║❒ *WaGroup:* _https://chat.whatsapp.com/FF6YuOZTAVB6Lu65cnY5BN_
-║❒ *WaChannel:* _https://whatsapp.com/channel/0029VaDK8ZUDjiOhwFS1cP2j_
-║❒ *Plugins:* _https://github.com/Itxxwasi 
+║❒ *Repo:* _https://github.com/romeomdbot/Romeo-Md_
+║❒ *WaGroup:* _htt_
+║❒ *WaChannel:* _h_
+║❒ *Plugins:* _
 ╚════════════════════════╝
 _____________________________________
 	
